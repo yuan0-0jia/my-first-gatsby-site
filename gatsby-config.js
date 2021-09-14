b07376404d04c05a6532adb9273a1303ece23daf
yuan0-0jia <yuan010125@outlook.com>
2021-09-14 16:06:36 -0700

    reset project

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "What would you like to call your site?✔ · My First Gatsby Site",
+  },
+  plugins: [],
+};
