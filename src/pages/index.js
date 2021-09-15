eac658d444239ec9a497092b14006b29a852e893
yuan0-0jia <yuan010125@outlook.com>
2021-09-15 11:14:07 -0700

    Finish Part 2:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
 // Step 1: Import React. This lets you use JSX inside your .js file.
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 /* Step 2: Define your component. Note that your
 component name should start with a capital letter. */
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
   )
 }
 /* Step 3: Export your component so it
